90c6ccdfa54144ce210aefff3f35f1300269e1ca
enelija <enel@gmx.at>
2013-11-04 22:38:36 +0100

    Initial empty project

---
+++ src/DarkRoomTracking.cpp
@@ -0,0 +1,61 @@
+#include "DarkRoomTracking.h"
+
+//--------------------------------------------------------------
+void DarkRoomTracking::setup(){
+
+}
+
+//--------------------------------------------------------------
+void DarkRoomTracking::update(){
+
+}
+
+//--------------------------------------------------------------
+void DarkRoomTracking::draw(){
+
+}
+
+//--------------------------------------------------------------
+void DarkRoomTracking::keyPressed(int key){
+
+}
+
+//--------------------------------------------------------------
+void DarkRoomTracking::keyReleased(int key){
+
+}
+
+//--------------------------------------------------------------
+void DarkRoomTracking::mouseMoved(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void DarkRoomTracking::mouseDragged(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void DarkRoomTracking::mousePressed(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void DarkRoomTracking::mouseReleased(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void DarkRoomTracking::windowResized(int w, int h){
+
+}
+
+//--------------------------------------------------------------
+void DarkRoomTracking::gotMessage(ofMessage msg){
+
+}
+
+//--------------------------------------------------------------
+void DarkRoomTracking::dragEvent(ofDragInfo dragInfo){ 
+
+}
